832329a5575df6510a0cb1482c327080f6080eaf
root <root@localhost.localdomain>
2018-07-10 20:42:12 +0800

    Authentication account changed.

--- fireArd.py
+++ fireArd.py
@@ -13,8 +13,8 @@ config = {
 firebase = pyrebase.initialize_app(config)
 
 #Authentication
-email = "elliot@fsociety.com"
-password = "root@fsoc.py~#"
+email = "user@firebase.com"
+password = "user1234"
 
 # Get a reference to the auth service
 auth = firebase.auth()
@@ -25,19 +25,15 @@ user = auth.sign_in_with_email_and_password(email, password)
 # Get a reference to the database service
 db = firebase.database()
 
-gr = db.child("colors").child("green").get()
-bl = db.child("colors").child("blue").get()
-re = db.child("colors").child("red").get()
+gr = db.child("colors").child("green").get(user['idToken'])
+bl = db.child("colors").child("blue").get(user['idToken'])
+re = db.child("colors").child("red").get(user['idToken'])
 
-# print(gr.val())
-
-
-
-# Port for Arduino
+# Ports for Arduino
 ## Windows ##
-board = Arduino('COM5')
+# board = Arduino('COM5')
 ## Linux ##
-#board = Arduino('/dev/ttyACM0')
+board = Arduino('/dev/ttyACM0')
 
 # Pin Code
 r = 13
@@ -45,17 +41,22 @@ g = 12
 b = 8
 
 
+
 if gr.val() == 1:
 	board.digital[g].write(1)
 	print("Green is On")
-elif bl.val() == 1:
+else:
+	board.digital[g].write(0)
+	print("Green is Off")
+if bl.val() == 1:
 	board.digital[b].write(1)
 	print("Blue is On")
-elif re.val() == 1:
+else:
+	board.digital[b].write(0)
+	print("Blue is Off")
+if re.val() == 1:
 	board.digital[r].write(1)
 	print("Red is On")
 else:
-	board.digital[g].write(1)
-	board.digital[r].write(1)
-	board.digital[b].write(1)
-	print("Off")
+	board.digital[r].write(0)
+	print("Red is Off")
