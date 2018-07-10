aeeda18482467de011705b642dce1a3affe92530
mboy1011 <lyjieme@gmail.com>
2018-07-10 17:29:32 +0800

    Windows Port Added

--- fireArd.py
+++ fireArd.py
@@ -1,43 +1,61 @@
-# import pyrebase
+import pyrebase
 from pyfirmata import Arduino, util
 from time import sleep
 
 # # Credentials for Firebase Account
-# config = {
-# 	"apiKey": "AIzaSyA5qDzIcT3nsfha6yE4lH_7rbf3JjoQkHM",
-# 	"authDomain": "colors-45ade.firebaseapp.com",
-# 	"databaseURL": "https://colors-45ade.firebaseio.com",
-# 	"storageBucket": "colors-45ade.appspot.com"
-# }
+config = {
+	"apiKey": "AIzaSyA5qDzIcT3nsfha6yE4lH_7rbf3JjoQkHM",
+	"authDomain": "colors-45ade.firebaseapp.com",
+	"databaseURL": "https://colors-45ade.firebaseio.com",
+	"storageBucket": "colors-45ade.appspot.com"
+}
 
-# firebase = pyrebase.initialize_app(config)
+firebase = pyrebase.initialize_app(config)
 
-# #Authentication
-# email = "elliot@fsociety.com"
-# password = "root@fsoc.py~#"
+#Authentication
+email = "elliot@fsociety.com"
+password = "root@fsoc.py~#"
 
-# # Get a reference to the auth service
-# auth = firebase.auth()
+# Get a reference to the auth service
+auth = firebase.auth()
 
-# # Log the user in
-# user = auth.sign_in_with_email_and_password(email, password)
+# Log the user in
+user = auth.sign_in_with_email_and_password(email, password)
 
-# # Get a reference to the database service
-# db = firebase.database()
+# Get a reference to the database service
+db = firebase.database()
 
-# c = db.child("colors").get()
+gr = db.child("colors").child("green").get()
+bl = db.child("colors").child("blue").get()
+re = db.child("colors").child("red").get()
 
-# print(c.val())
+# print(gr.val())
 
 
 
 # Port for Arduino
-board = Arduino('/dev/ttyACM0')
+## Windows ##
+board = Arduino('COM5')
+## Linux ##
+#board = Arduino('/dev/ttyACM0')
 
-# Pin 13 On
-board.digital[13].write(1)
+# Pin Code
+r = 13
+g = 12
+b = 8
 
-sleep(1)
 
-# Pin 13 Off
-board.digital[13].write(0)
+if gr.val() == 1:
+	board.digital[g].write(1)
+	print("Green is On")
+elif bl.val() == 1:
+	board.digital[b].write(1)
+	print("Blue is On")
+elif re.val() == 1:
+	board.digital[r].write(1)
+	print("Red is On")
+else:
+	board.digital[g].write(1)
+	board.digital[r].write(1)
+	board.digital[b].write(1)
+	print("Off")
